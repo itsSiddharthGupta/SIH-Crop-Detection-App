0006ebaf308044a6a50e419f5357842dab2ee608
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2020-01-29 13:06:38 +0530

    Add splash screen and update theme.

---
+++ app/src/main/java/com/kars/sihpoc/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.kars.sihpoc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
