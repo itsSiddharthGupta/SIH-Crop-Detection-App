30e607fd1a9c895600fd38e57da42726134d0cbd
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2020-01-29 13:08:15 +0530

    Implement image classification.

--- app/src/main/java/com/kars/sihpoc/MainActivity.java
+++ app/src/main/java/com/kars/sihpoc/MainActivity.java
@@ -1,167 +1,81 @@
 package com.kars.sihpoc;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
-import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.location.Location;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CAMERA = 101, REQUEST_LOCATION = 102;
-    private ImageView imgCropPic;
-    private String imgPath;
-    private TextView txtLocation;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_CAMERA && resultCode==RESULT_OK && data!=null){
-            Log.i("ACTIVITY-RESULT", true+"");
-        }
-        setPic();
-    }
+    private Button btnIdentify;
+    private TextView txtWeatherAndLocation;
+    private static final int REQUEST_CAMERA = 101;
+    private String imgPath = null;
+    private Uri imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imgCropPic = findViewById(R.id.imgCropPic);
-        txtLocation = findViewById(R.id.txtLocation);
-        requestPermissions();
-        Button btnTakePic = findViewById(R.id.btnTakePic);
-        btnTakePic.setOnClickListener(new View.OnClickListener() {
+        btnIdentify = findViewById(R.id.btnIdentify);
+        txtWeatherAndLocation = findViewById(R.id.txtLocation);
+        btnIdentify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(intent.resolveActivity(getPackageManager())!=null){
-                    File imgFile = null;
-                    try{
-                        imgFile = createImageFile();
-                    }catch (IOException e){
-                        e.printStackTrace();
-                    }
-                    if (imgFile != null) {
-                        Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
-                                "com.kars.android.fileprovider",
-                                imgFile);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(intent, REQUEST_CAMERA);
-                    }
-
-                }
+                openCameraIntent();
             }
         });
-
-
     }
 
-    private File createImageFile() throws IOException{
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        imgPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void setPic() {
-        // Get the dimensions of the View
-        int targetW = imgCropPic.getWidth();
-        int targetH = imgCropPic.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-        Bitmap bitmap = BitmapFactory.decodeFile(imgPath, bmOptions);
-        Log.e("IMAGE_PATH", imgPath);
-        imgCropPic.setImageBitmap(bitmap);
+    // opens camera for user
+    private void openCameraIntent(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        imageURI= getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_CAMERA);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_LOCATION:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this, "Location provided!", Toast.LENGTH_SHORT).show();
-                    operateLocation();
-                } else {
-                    Toast.makeText(this, "Grant Location Permission from settings.", Toast.LENGTH_SHORT).show();
-                }
-        }
-    }
-
-    private void requestPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION);
-        } else {
-            Toast.makeText(MainActivity.this, "Location permission Granted!", Toast.LENGTH_SHORT).show();
-            operateLocation();
-        }
-    }
-
-    private void operateLocation(){
-        FusedLocationProviderClient locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        locationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location!=null){
-                    double lat = location.getLatitude();
-                    double longitude = location.getLongitude();
-                    txtLocation.setText("Latitude : " + lat + "\nLongitude : " + longitude);
-                }else{
-                    Log.e("LOCATION", "location is null");
-                    txtLocation.setText("location is null");
-                }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'Classify' activity
+        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            try {
+                Uri source_uri = imageURI;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(MainActivity.this);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
+        // if cropping acitivty is finished, get the resulting cropped image uri and send it to 'Classify' activity
+        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+            imageURI = Crop.getOutput(data);
+            Intent i = new Intent(MainActivity.this, Classify.class);
+            // put image data in extras to send
+            i.putExtra("resID_uri", imageURI);
+            // put filename in extras
+            i.putExtra("chosen", "model.tflite");
+            startActivity(i);
+        }
     }
 }
