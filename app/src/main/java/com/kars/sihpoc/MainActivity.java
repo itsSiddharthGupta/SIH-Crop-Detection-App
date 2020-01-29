3033879d4601166f3a77e48550e1ff849fc674b0
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2020-01-29 14:43:22 +0530

    Add Location capture.(Partial)

--- app/src/main/java/com/kars/sihpoc/MainActivity.java
+++ app/src/main/java/com/kars/sihpoc/MainActivity.java
@@ -2,13 +2,19 @@ package com.kars.sihpoc;
 
 import android.Manifest;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,23 +26,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
-
-public class MainActivity extends AppCompatActivity {
+//Todo Location still not stable
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private Button btnIdentify;
-    private TextView txtWeatherAndLocation;
-    private static final int REQUEST_CAMERA = 101, REQUEST_STORAGE_PERMISSION = 102, REQUEST_LOCATION_PERMISSION = 103;
+    private static final int REQUEST_CAMERA = 101, REQUEST_STORAGE_PERMISSION = 102,
+            REQUEST_LOCATION_PERMISSION = 103, REQUEST_CHECK_SETTINGS = 104;
     private String imgPath = null;
     private Uri imageURI;
+    private TextView txtWeatherLocation;
+    public static FusedLocationProviderClient locationProviderClient;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnIdentify = findViewById(R.id.btnIdentify);
-        txtWeatherAndLocation = findViewById(R.id.txtLocation);
+        txtWeatherLocation = findViewById(R.id.txtWeatherLocation);
         requestLocationPermission();
         btnIdentify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,21 +66,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void requestStoragePermission(){
+
+    private void requestStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
                 && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_STORAGE_PERMISSION);
+        } else {
+            openCameraIntent();
         }
     }
 
-    private void requestLocationPermission(){
+    private void requestLocationPermission() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_PERMISSION);
+        } else {
+            displayLocationSettingsRequest(this);
         }
     }
 
@@ -71,12 +97,14 @@ public class MainActivity extends AppCompatActivity {
             if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 Toast.makeText(getApplicationContext(), "This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG).show();
                 finish();
+            } else {
+                displayLocationSettingsRequest(this);
             }
-        }else if(requestCode == REQUEST_STORAGE_PERMISSION){
+        } else if (requestCode == REQUEST_STORAGE_PERMISSION) {
             if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 Toast.makeText(getApplicationContext(), "This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG).show();
                 finish();
-            }else{
+            } else {
                 openCameraIntent();
             }
         }
@@ -120,6 +148,91 @@ public class MainActivity extends AppCompatActivity {
             // put filename in extras
             i.putExtra("chosen", "model.tflite");
             startActivity(i);
+        } else if (requestCode == REQUEST_CHECK_SETTINGS && resultCode == RESULT_OK)
+            showLocation();
+    }
+
+    private void displayLocationSettingsRequest(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                String TAG = "Location-Settings";
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.e(TAG, "All location settings are satisfied.");
+                        showLocation();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.e(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.e(TAG, "PendingIntent unable to execute request.");
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.e(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                        break;
+                }
+            }
+        });
+    }
+    private void showLocation(){
+        locationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!=null){
+                    double lat = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    txtWeatherLocation.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+                    Log.e("Location","Latitude : " + lat + "\nLongitude : " + longitude);
+                }else{
+                    Log.e("LOCATION", "location is null");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        txtWeatherLocation.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
     }
 }
