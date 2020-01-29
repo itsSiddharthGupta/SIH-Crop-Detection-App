854e13a18458d285b2748ef4f7c3df6c400de6da
itsSiddharthGupta <36455409+itsSiddharthGupta@users.noreply.github.com>
2020-01-29 13:31:23 +0530

    Request permissions implement.

--- app/src/main/java/com/kars/sihpoc/MainActivity.java
+++ app/src/main/java/com/kars/sihpoc/MainActivity.java
@@ -1,17 +1,24 @@
 package com.kars.sihpoc;
 
+import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.soundcloud.android.crop.Crop;
 
@@ -20,7 +27,7 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
     private Button btnIdentify;
     private TextView txtWeatherAndLocation;
-    private static final int REQUEST_CAMERA = 101;
+    private static final int REQUEST_CAMERA = 101, REQUEST_STORAGE_PERMISSION = 102, REQUEST_LOCATION_PERMISSION = 103;
     private String imgPath = null;
     private Uri imageURI;
 
@@ -30,21 +37,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnIdentify = findViewById(R.id.btnIdentify);
         txtWeatherAndLocation = findViewById(R.id.txtLocation);
+        requestLocationPermission();
         btnIdentify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openCameraIntent();
+                requestStoragePermission();
             }
         });
     }
+    private void requestStoragePermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        }
+    }
+
+    private void requestLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    // checks that the user has allowed all the required permission of read and write and camera. If not, notify the user and close the application
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(), "This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }else if(requestCode == REQUEST_STORAGE_PERMISSION){
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(), "This application needs read, write, and camera permissions to run. Application now closing.", Toast.LENGTH_LONG).show();
+                finish();
+            }else{
+                openCameraIntent();
+            }
+        }
+    }
 
     // opens camera for user
-    private void openCameraIntent(){
+    private void openCameraIntent() {
         ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.TITLE, "New Picture");
         values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
         // tell camera where to store the resulting picture
-        imageURI= getContentResolver().insert(
+        imageURI = getContentResolver().insert(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
@@ -57,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'Classify' activity
-        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
             try {
                 Uri source_uri = imageURI;
                 Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
@@ -68,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         // if cropping acitivty is finished, get the resulting cropped image uri and send it to 'Classify' activity
-        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+        else if (requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK) {
             imageURI = Crop.getOutput(data);
             Intent i = new Intent(MainActivity.this, Classify.class);
             // put image data in extras to send
